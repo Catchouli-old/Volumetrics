707047cd18e1834d165712eded308e49af8bb3d0
Caitlin Wilks <Catchouli@users.noreply.github.com>
2017-05-11 17:45:06 +0100

    Update 1.2.glsl

--- exercises/1/1.2.glsl
+++ exercises/1/1.2.glsl
@@ -6,9 +6,18 @@ bool pointInCircle(vec2 point, vec2 circlePos, float radius)
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
+    // calculate aspect ratio
     float aspect = iResolution.x / iResolution.y;
+    
+    // convert coords to (-1, 1) space, this makes it easier to correct for aspect ratio
+    // without offsetting the image, and will result in a coordinate space for the image
+    // which goes from -1 to 1 in the x axis, and slightly less in the y axis to correct
+    // the units so they're equally sized in each axis
     vec2 uv = fragCoord.xy / iResolution.xy;
-    if (pointInCircle(vec2(uv.x, uv.y / aspect), vec2(0.5, 0.5 / aspect), 0.25))
+    vec2 coord = 2.0 * uv - 1.0;
+    coord.y /= aspect;
+    
+    if (pointInCircle(coord, vec2(0, 0), 0.25))
         fragColor = vec4(1.0, 1.0, 1.0, 1.0);
     else
         fragColor = vec4(0.0);
